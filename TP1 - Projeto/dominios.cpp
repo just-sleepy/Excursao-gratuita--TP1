152abbfc4bba2cfdc0bd59d86d20b71c745a225d
Felipedbbsb <80411335+Felipedbbsb@users.noreply.github.com>
2022-02-06 01:28:33 -0300

    Update dominios.cpp
    metodos de titulo add

--- TP1 - Projeto/dominios.cpp
+++ TP1 - Projeto/dominios.cpp
@@ -1,5 +1,57 @@
 #include "dominios.h"
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+using  namespace std;
 
 // --------------------------------------------------------------------------
-// Implementações de métodos de classe domínio.
+// ImplementaÃ§Ãµes de mÃ©todos de classe domÃ­nio.
+
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+//Titulo
+
+void Titulo::validar(string valor){
+    int len = valor.lengh()
+    int ponto, espaco, valido = 1;
+    string p1("."),p2(" ");
+
+    for (int i = 0; s[i] != '\0'; i++){
+        string c;
+        c = s[i];
+        if (std::isdigit(s[i]) == 1){
+            valido = 0;
+        }
+        else if (bool(c == p1) == 1){
+            ++ ponto;
+            cout <<ponto << "entrou";
+            espaco = 0;  //zera a contagem de espacos
+            if (ponto == 2){
+                valido = 0;
+            }
+        }
+        else if(bool(c == p2) == 1){
+            ++ espaco ;
+            ponto = 0;//zera a contagem de pontos
+            if (espaco == 2){
+                valido = 0;
+            }
+        }
+        else{
+            ponto = 0;
+            espaco = 0;
+        }
+    }
+
+    if((len < 5 ) || (len > 20)){
+        throw invalid_argument("Argumento invalido.");
+    }
+    else if (valido == 0){
+        throw invalid_argument("Argumento invalido.");
+    }
+void Titulo::setValor(string valor) {
+    validar(valor);
+    this->valor = valor;
+}
 
