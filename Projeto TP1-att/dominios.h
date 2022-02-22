525b140e2579ed1c71e26cbf64273393113475d8
Felipedbbsb <80411335+Felipedbbsb@users.noreply.github.com>
2022-02-22 20:51:44 -0300

    Update dominios.h

--- Projeto TP1-att/dominios.h
+++ Projeto TP1-att/dominios.h
@@ -109,12 +109,36 @@ inline string Nome::getValor() const{
 //----------------------------------------------------------------------- */
 //Matricula: 202021749
 // Felipe Dantas Borges
+///
+/// Padr&atilde;o para representa&ccedil;&atilde;o de email.
+///
+/// Regras de formato:
+///
+/// - C&oacute;digo &eacute; v&aacute;lido caso siga o formato de parte-local@dominio.
+///
+
 class Email {
     private:
         string valor;                      // Atributo para armazenar valor.
         void validar(string);              // Método para validar valor.
     public:
+        ///
+/// Armazena email.
+///
+/// @param valor email.
+///
+/// @return
+///
+/// - Retorna true  caso email tenha sido armazenado.
+/// - Retorna false caso email n&atilde;o tenha sido armazenado por ser inv&aacute;lido.
+///
         void setValor(string);             // Método para atribuir valor.
+
+ ///
+/// Retorna email.
+///
+/// @return email
+///
         string getValor() const;           // Método para recuperar valor.
 };
 
@@ -125,12 +149,35 @@ inline string Email::getValor() const{
 //----------------------------------------------------------------------- */
 //Matricula: 202021749
 // Felipe Dantas Borges
+///
+/// Padr&atilde;o para representa&ccedil;&atilde;o de data.
+///
+/// Regras de formato:
+///
+/// - C&oacute;digo &eacute; v&aacute;lido caso siga o formato de DD-MES-AAAA.
+///
+
 class Data {
     private:
         string valor;                      // Atributo para armazenar valor.
         void validar(string);              // Método para validar valor.
     public:
+        /// Armazena data.
+///
+/// @param valor data.
+///
+/// @return
+///
+/// - Retorna true  caso data tenha sido armazenado.
+/// - Retorna false caso data n&atilde;o tenha sido armazenado por ser inv&aacute;lido.
+
         void setValor(string);             // Método para atribuir valor.
+
+///
+/// Retorna data.
+///
+/// @return data
+///
         string getValor() const;           // Método para recuperar valor.
 };
 
@@ -142,12 +189,34 @@ inline string Data::getValor() const{
 //----------------------------------------------------------------------- */
 //Matricula: 202021749
 // Felipe Dantas Borges
+///
+/// Padr&atilde;o para representa&ccedil;&atilde;o de endereço.
+///
+/// Regras de formato:
+///
+/// - C&oacute;digo &eacute; v&aacute;lido caso tenha de 0 a 20 caracteres.
+///
 class Endereco {
     private:
         string valor;                      // Atributo para armazenar valor.
         void validar(string);              // Método para validar valor.
     public:
+/// Armazena endereço.
+///
+/// @param valor endereço.
+///
+/// @return
+///
+/// - Retorna true  caso endereço tenha sido armazenado.
+/// - Retorna false caso endereço n&atilde;o tenha sido armazenado por ser inv&aacute;lido.
+
         void setValor(string);             // Método para atribuir valor.
+
+///
+/// Retorna endereço.
+///
+/// @return endereço
+///
         string getValor() const;           // Método para recuperar valor.
 };
 
@@ -159,12 +228,36 @@ inline string Endereco::getValor() const{
 //----------------------------------------------------------------------- */
 //Matricula: 202021749
 // Felipe Dantas Borges
+///
+/// Padr&atilde;o para representa&ccedil;&atilde;o de t&iacute;tulo.
+///
+/// Regras de formato:
+///
+/// - C&oacute;digo &eacute; v&aacute;lido caso tenha de 5 a 20 letras.
+///
+
+
 class Titulo {
     private:
         string valor;
         void validar(string);
     public:
+/// Armazena t&iacute;tulo.
+///
+/// @param valor t&iacute;tulo.
+///
+/// @return
+///
+/// - Retorna true  caso t&iacute;tulo tenha sido armazenado.
+/// - Retorna false caso t&iacute;tulo n&atilde;o tenha sido armazenado por ser inv&aacute;lido.
+
         void setValor(string);
+
+///
+/// Retorna t&iacute;tulo
+///
+/// @return t&iacute;tulo
+///
         string getValor() const;
 };
 inline string Titulo::getValor() const{
